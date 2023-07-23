d4d7c981c8ef9fe5b38008999b75e54ed1f4772a
unknown <sharmag8121@gmail.com>
2023-07-23 22:03:05 +0530

    prop drilling with component composition

--- src/App.js
+++ src/App.js
@@ -52,21 +52,29 @@ const average = (arr) =>
 
 export default function App() {
   const [movies, setMovies] = useState(tempMovieData);
-
+  // prop drilling
   return (
     <>
-      <NavBar movies={movies} />
-      <Main movies={movies} />
+      <NavBar>
+        <Search />
+        <NumResults movies={movies} />
+      </NavBar>
+      <Main>
+        <ListBox>
+          <MovieList movies={movies} />
+        </ListBox>
+        <WatchedBox />
+      </Main>
     </>
   );
 }
 
-function NavBar({ movies }) {
+// prop drilling
+function NavBar({ children }) {
   return (
     <nav className="nav-bar">
       <Logo />
-      <Search />
-      <NumResults movies={movies} />
+      {children}
     </nav>
   );
 
@@ -96,14 +104,13 @@ function Search() {
   />);
 }
 
-function Main({ movies }) {
+function Main({ children }) {
   return (<main className="main">
-    <ListBox movies={movies} />
-    <WatchedBox />
+    {children}
   </main>);
 }
 
-function ListBox({ movies }) {
+function ListBox({ children }) {
   const [isOpen1, setIsOpen1] = useState(true);
 
   return (<div className="box">
@@ -113,9 +120,9 @@ function ListBox({ movies }) {
     >
       {isOpen1 ? "–" : "+"}
     </button>
-    {isOpen1 && (
-      <MovieList movies={movies} />
-    )}
+    {isOpen1 &&
+      children
+    }
   </div>);
 }
 
